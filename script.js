f013c9085c88f3cd036db6bc1fd612b860930671
SalvadorB-2 <barrigas252@gmail.com>
2026-03-24 13:45:42 -0700

    minor updates

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navbar.classList.toggle("active");
 });
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768) {
-    navbar.style.display = "block";
-  } else {
-    navbar.style.display = "none";
-  }
-});
